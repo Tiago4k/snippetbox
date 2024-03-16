551cd26e330f0a94719a7e85a80a49908e1b7541
Tiago Ramalho <tiago4k@gmail.com>
2024-03-16 21:59:55 +0000

    adding sessions

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -12,15 +12,17 @@ func (app *application) routes() http.Handler {
 	fileServer := http.FileServer(http.Dir("./ui/static/"))
 	mux.Handle("GET /static/", http.StripPrefix("/static", fileServer))
 
-	mux.HandleFunc("GET /{$}", app.home)
-	mux.HandleFunc("GET /snippet/view/{id}", app.snippetView)
-	mux.HandleFunc("GET /snippet/create", app.snippetCreate)
-	mux.HandleFunc("POST /snippet/create", app.snippetCreatePost)
+	dynamic := alice.New(app.sessionManager.LoadAndSave)
+
+	mux.Handle("GET /{$}", dynamic.ThenFunc(app.home))
+	mux.Handle("GET /snippet/view/{id}", dynamic.ThenFunc(app.snippetView))
+	mux.Handle("GET /snippet/create", dynamic.ThenFunc(app.snippetCreate))
+	mux.Handle("POST /snippet/create", dynamic.ThenFunc(app.snippetCreatePost))
 
 	// catch all
-	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+	mux.Handle("/", dynamic.ThenFunc(func(w http.ResponseWriter, r *http.Request) {
 		app.notFound(w)
-	})
+	}))
 
 	// middleware to be applied to every request
 	standard := alice.New(app.recoverPanic, app.logRequest, secureHeaders)
